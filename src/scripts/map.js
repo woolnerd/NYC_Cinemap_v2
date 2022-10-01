018e5fcb0fe940873d6951bfd7d7cf84efff7cb5
DavidWoolner <davidwoolner@voray.com>
2022-10-01 16:35:38 -0400

    Last attempt with api key

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -2,7 +2,8 @@ import mapboxgl from "mapbox-gl";
 import { geoJSON } from "../assets/scene_data";
 import { addToGallery } from "../index";
 import { DataStore } from "../components/gallery";
-const apiKey = 'pk.eyJ1IjoiZGF2aWR3b29sbmVyIiwiYSI6ImNrczliam40MzB0YTIydm9ja2x3NDN5cnQifQ.WodjI99jg0lWF31OhaXCFA';
+require("dotenv").config();
+const apiKey = process.env.MAP_BOX_API_KEY
 mapboxgl.accessToken = apiKey;
 
 export const imdbID = "tt0075686";
