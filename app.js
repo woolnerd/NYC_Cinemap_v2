23b8b804dadcbe02c52fae7a9abf7595db669af8
DavidWoolner <davidwoolner@voray.com>
2022-10-01 21:20:28 -0400

    Re-add api key to location.js

--- app.js
+++ app.js
@@ -40,4 +40,4 @@ app.get('/api', (request, response) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => null);
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
