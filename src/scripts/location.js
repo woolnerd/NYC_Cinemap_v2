59648d63c3e9f3e2062dc37fbff2514c0cf808d0
woolnerd <davidwoolner@gmail.com>
2024-08-11 08:57:16 -0400

    Update location api key

--- src/scripts/location.js
+++ src/scripts/location.js
@@ -1,7 +1,8 @@
-import { makeMap } from "./map";
+import { makeMap } from './map';
 
 export async function getLocation(address) {
-  const apiKey = 'pk.eyJ1IjoiZGF2aWR3b29sbmVyIiwiYSI6ImNrczliam40MzB0YTIydm9ja2x3NDN5cnQifQ.WodjI99jg0lWF31OhaXCFA';
+  const apiKey = process.env.MAP_BOX_API_KEY;
+
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${apiKey}`;
   const response = await fetch(url);
   return response;
@@ -10,28 +11,28 @@ export async function getLocation(address) {
 //search for location from input
 export function locationSearch(e) {
   e.preventDefault();
-  const locInput = document.querySelector("#loc-input");
+  const locInput = document.querySelector('#loc-input');
   const address = locInput.value;
 
-  const curLoc = document.querySelector("#cur-loc");
+  const curLoc = document.querySelector('#cur-loc');
   curLoc.innerHTML = `Search Location:  ${address}`;
 
-  locInput.value = "";
+  locInput.value = '';
 
   getLocation(address)
     .then((response) => {
       if (!response.ok) {
-        throw new Error("Network response was not ok");
+        throw new Error('Network response was not ok');
       }
       return response.json();
     })
     .then((data) => {
       const coords = data.features[0].geometry.coordinates;
       makeMap(coords[0], coords[1]);
-      const curLoc = document.querySelector("#cur-loc");
+      const curLoc = document.querySelector('#cur-loc');
       curLoc.innerHTML = `Search Location: ${address}`;
     })
     .catch((error) => {
-      console.error("Problem intercepted: ", error);
+      console.error('Problem intercepted: ', error);
     });
 }
